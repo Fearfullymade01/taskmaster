045021b90c7b5b503969d274acf03f71092d9fbb
fearfullymade01 <gegbeyemi.poise@gmail.com>
2026-01-19 22:30:55 +0000

    Add task homepage with To Do and Done tables, views, and static files

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Task
+
+
+def index(request):
+	to_do = Task.objects.filter(completed=False).order_by('-due_date', '-created_at')
+	done = Task.objects.filter(completed=True).order_by('-due_date', '-created_at')
+	return render(request, 'tasks/index.html', {
+		'to_do': to_do,
+		'done': done,
+	})
 
 # Create your views here.
